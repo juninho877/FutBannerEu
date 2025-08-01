a44e168bcccbd2dae0d3bab4e6cb7c6fb9b3b4a3
juninho877 <alessandrosandrinho877@gmail.com>
2025-08-01 16:37:39 -0300

    Updated buy_credits.php

--- admin/store_credit_payment_success.php
+++ admin/store_credit_payment_success.php
@@ -14,6 +14,7 @@ if (isset($_POST['message']) && !empty($_POST['message'])) {
     // Limpar dados do pagamento
     unset($_SESSION['credit_payment_qr_code']);
     unset($_SESSION['credit_payment_pix_code']);
+    unset($_SESSION['credit_payment_pix_code']);
     unset($_SESSION['credit_payment_created_at']);
     unset($_SESSION['credit_payment_id']);
     unset($_SESSION['credit_payment_amount']);
